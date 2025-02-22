1f6b32d6d32c4baf10ca6595eb04f8146acd448c
Karan <mrkaran2k5@gmail.com>
2025-02-22 19:29:50 +0530

    Analytics for Live

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider>
     <App />
+    <Analytics />
   </ChakraProvider>
 );
